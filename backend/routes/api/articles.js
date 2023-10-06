1429b94b21824a2f9f388f24b36db70a7606cf43
jamjamin <benjamjamin02@gmail.com>
2023-10-06 20:08:51 +1300

    API call for getting all articles

--- backend/routes/api/articles.js
+++ backend/routes/api/articles.js
@@ -6,4 +6,13 @@ const Article = require('../../models/Article');
 // Router Test
 router.get('/test', (req, res) => res.send('Sup'));
 
+// @route GET api/books
+// @description Get all articles 
+// @access Public
+router.get('/', (req, res) => {
+    Article.find()
+        .then(articles => res.json(articles))
+        .catch(err => res.status(404).json({ noarticlesfound: 'No articles found'}))
+});
+
 module.exports = router;
